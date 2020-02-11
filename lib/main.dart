1250ada840e5d0fa6218b835e8b3efaea64f14fe
OzanOcak <ozanocak@gmail.com>
2020-02-11 11:51:27 -0500

    with button and better design

--- lib/main.dart
+++ lib/main.dart
@@ -30,16 +30,28 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: Colors.teal.shade300,
         appBar: AppBar(title: Text('my app'),backgroundColor: Colors.teal.shade900,),
         body: Center(
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(child: FlatButton(              
-                child: Image(image: AssetImage('images/dice$leftDice.png')), 
-                onPressed: () {rollDice();},)),
-              Expanded(child: FlatButton(
-                child: Image(image: AssetImage('images/dice$rightDice.png')), 
-                onPressed: () {rollDice();},))
-            ],
+          child:Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Spacer(flex:3),
+             Row(
+              children: <Widget>[
+                Spacer(),
+                Expanded(child: Image(image: AssetImage('images/dice$leftDice.png'))), 
+                Spacer(),
+                Expanded(child: Image(image: AssetImage('images/dice$rightDice.png'))), 
+                Spacer(),
+              ],
+            ),
+            Spacer(flex:1),
+            FlatButton(
+              padding:EdgeInsets.fromLTRB(5.0,2.0,5.0,2.0),
+              child: Text("Roll'em"), 
+              onPressed: () {rollDice();},
+              color:Colors.teal.shade200,
+              textColor:Colors.white ),
+            Spacer(flex:2)
+           ],
           ),
         ),
       )
